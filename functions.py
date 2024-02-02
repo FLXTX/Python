fa36885158f4712283b0a002d7401f2ff0340ec4
Fred <fred.ruubel@gmail.com>
2024-02-02 18:15:46 +0200

    Spelling mistakes, added functions.py

--- functions.py
+++ functions.py
@@ -1,8 +1,31 @@
 
-#Tuple is a structure I can store multiple pieces of information, similar to lists
-#Tuple can't be changed or modified
 
 
-coordinates = (4, 5)
-print(coordinates[0])
 
+
+# def keyword is required to use functions
+
+def say_hi() :          # all the code coming after this line is in the function and it HAS TO BE INDENTED!!
+    print("hello user")
+                               
+
+print("top")
+say_hi()                        # to access the function I have to call it
+print("bottom") 
+
+def say_hi(name, age) :          # A parameter is a piece of info I give to the function
+    print("Hello " + name + ", you are " + age + " old.")
+
+say_hi("Mike", "35")  
+say_hi("Steve", "55")            
+
+def cube(num):          # return keyword returns information from a function
+    return num*num*num  
+
+print(cube(3))
+
+def cube(num):          
+    return num*num*num  
+
+result = cube(4)
+print(result)
--- lists and tuples.py
+++ lists and tuples.py
@@ -58,4 +58,4 @@ print(friends)
 coordinates = (4, 5)
 print(coordinates[0])
 
-coordinates = {(4, 5), (6, 7), (80, 90)}    # You can also make a list of tuples
+coordinates = [(4, 5), (6, 7), (80, 90)]    # You can also make a list of tuples
