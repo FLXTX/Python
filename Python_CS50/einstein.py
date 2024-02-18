84836203ca80c5652f83a125e97cbfbad4ccfa28
Fred <fred.ruubel@gmail.com>
2024-02-18 17:24:54 +0200

    Added einstein.py

---
+++ Python_CS50/einstein.py
@@ -0,0 +1,11 @@
+
+
+
+def main():
+    mass = int(input("Enter mass in kilograms: "))
+    lightspeed = 300000000
+    energy = mass * lightspeed ** 2
+    print("The awnser is " + str(energy) + " joules")
+    return energy
+
+main()
