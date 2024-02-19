4adae305aa577fd88a649fc459f4ceb7d77483a5
Fred <fred.ruubel@gmail.com>
2024-02-19 17:53:37 +0200

    Implemented a few design changes

--- Python_CS50/DeepThougths.py
+++ Python_CS50/DeepThougths.py
@@ -1,4 +1,8 @@
+# In deep.py, implement a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, 
+# outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. 
+# Otherwise output No.
 
+Hints
 
 def main():
     question = input("What is the Awnser to the Great Question on Life, the Universe and Everything? ")
--- Python_CS50/playback.py
+++ Python_CS50/playback.py
@@ -1,4 +1,5 @@
-
+# In a file called playback.py, implement a program in Python that prompts the user 
+# for input and then outputs that same input, replacing each space with ... (i.e., three periods).
 
 phrase = input("Write something here: ")
 new_phrase = phrase.replace(" ", "...")
