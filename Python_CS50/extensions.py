c580f5da1f1efbbe7580f1000fb9ecb264db1fef
Fred <fred.ruubel@gmail.com>
2024-02-27 20:40:07 +0200

    Finished extensions.py

--- Python_CS50/extensions.py
+++ Python_CS50/extensions.py
@@ -11,5 +11,23 @@
 # If the file’s name ends with some other suffix or has no suffix at all, output application/octet-stream instead, which is a common default.
 
 def main():
-    file_name = input("Name your file: ")
-    
+    file_name = input("Name your file: ").lower()
+    if file_name.endswith(".gif"):
+        print("image/gif")
+    elif file_name.endswith(".jpg"):
+        print("image/jpg")
+    elif file_name.endswith(".jpeg"):
+        print("image/jpeg")
+    elif file_name.endswith(".png"):
+        print("image/png")
+    elif file_name.endswith(".pdf"):
+        print("portable document format/pdf")
+    elif file_name.endswith(".txt"):
+        print("text/txt")
+    elif file_name.endswith(".zip"):
+        print("zip archive/zip")
+    else:
+        print("application/octet-stream")
+ 
+
+main()
