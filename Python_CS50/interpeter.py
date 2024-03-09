a6027d655f8e59befcc059d272cb01f20df7d576
Fred <fred.ruubel@gmail.com>
2024-03-09 16:35:54 +0200

    Added interpeter.py

---
+++ Python_CS50/interpeter.py
@@ -0,0 +1,21 @@
+
+
+def calculator(x, y, z):
+    if y == "+":
+        return x + z
+    elif y == "-":
+        return x - z
+    elif y == "*":
+        return x * z
+    elif y =="/":
+        return x / z
+    
+def main():
+    expression = input("Let's start calculating: ")
+    x,y,z = expression.split()
+    x = float(x)
+    z = float(z)
+    result = calculator(x, y, z)
+    print(result)
+
+main()
