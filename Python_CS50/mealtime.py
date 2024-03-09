97bbd53adfd1d781c8494be63827e53074d49c8d
Fred <fred.ruubel@gmail.com>
2024-03-09 17:17:25 +0200

    Added mealtime.py

---
+++ Python_CS50/mealtime.py
@@ -0,0 +1,19 @@
+
+
+def main():
+    mealtime = input("What time is it now?: ")
+    if mealtime = list(range(7,8)):
+        print("It's breakfast time !!")
+    elif mealtime = list(range(12,13)):
+        print("It's lunch time !!!")
+    elif mealtime = list(range(18,19)):
+        print("It's dinner time !!!")
+    else:
+        print("No food right now !")
+
+def convert(time):
+    hours, minutes = time.split(":")
+    time_result =  
+
+if __name__ == "__main__":
+    main()
