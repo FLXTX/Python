4a07ab0d99abda72a91e2dc40ccb052c9d5b0fa1
Fred <fred.ruubel@gmail.com>
2024-03-10 17:11:30 +0200

    Added camel.py

---
+++ Python_CS50/camel.py
@@ -0,0 +1,21 @@
+
+
+
+def camel_to_snake(camel_case):
+    snake_case = ""
+    for char in camel_case:
+        if char.isupper():
+            snake_case += "_" + char.lower()
+        else:
+            snake_case += char
+    return snake_case
+
+
+def main():
+    camel_case = input("camelCase phrase:")
+    snake_case = camel_to_snake(camel_case)
+    print("snake_case:", snake_case)
+
+if __name__ == "__main__":
+    main()
+
